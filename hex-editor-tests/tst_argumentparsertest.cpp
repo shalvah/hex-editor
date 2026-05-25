eb13fe62b6fd10b3501e16362ca5eaec3e6cfe0e
Shalvah <diakon.ng@gmail.com>
2026-05-25 02:10:17 +0200

    ArgumentParser automated tests

---
+++ hex-editor-tests/tst_argumentparsertest.cpp
@@ -0,0 +1,24 @@
+#include <QtTest>
+#include "../argumentparser.h"
+
+class ArgumentParserTest : public QObject {
+    Q_OBJECT
+
+private slots:
+    void noArgs() {
+        // Simulate: program name only, no path given
+        char prog[] = "hex-editor";
+        char *argv[] = { prog };
+        QCOMPARE(ArgumentParser::parse(1, argv), QString());
+    }
+
+    void withArg() {
+        char prog[] = "hex-editor";
+        char path[] = "/tmp/test.bin";
+        char *argv[] = { prog, path };
+        QCOMPARE(ArgumentParser::parse(2, argv), QString("/tmp/test.bin"));
+    }
+};
+
+QTEST_MAIN(ArgumentParserTest)
+#include "tst_argumentparsertest.moc"
--- main.cpp
+++ main.cpp
@@ -2,6 +2,7 @@
 #include "mainwindow.h"
 
 #include <QApplication>
+#include <QDebug>
 
 int main(int argc, char *argv[])
 {
